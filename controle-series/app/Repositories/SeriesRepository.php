9d40b34e6575a988aba0e63272ef95cb1eece22a
Pedrogabriel152 <105892538+Pedrogabriel152@users.noreply.github.com>
2023-03-03 13:36:08 -0300

    Progredindo com Laravel

---
+++ controle-series/app/Repositories/SeriesRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Series;
+use App\Models\Season;
+use App\Models\Episode;
+use App\Http\Requests\SeriesFormRequest;
+
+class SeriesRepository
+{
+    public function add(SeriesFormRequest $request): Series {
+        return DB::transaction(function () use ($request) {
+            $serie = Series::create($request->all());
+
+            $seasons = [];
+            for($i = 1; $i <= intval($request->seasonsQty); $i++) {
+                $seasons[] = [
+                    'series_id' => $serie->id,
+                    'number' => $i,
+                ];
+            }
+            
+            Season::insert($seasons);
+            $episodes = [];
+            foreach ($serie->seasons as $season) {
+                for ($j=1; $j <= $request->espisodesQty; $j++) { 
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $j
+                    ];
+                }
+            }
+
+            Episode::insert($episodes);
+
+            return $serie;
+        });
+    }
+}
